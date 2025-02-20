b28b2e17f43aa3e2a7bbe6f66c367ccecac68ee0
Gustavo Erick <gustavoerickv@gmail.com>
2025-02-20 14:09:49 -0300

    strapi v1

---
+++ backend/src/api/visita/controllers/visita.ts
@@ -0,0 +1,7 @@
+/**
+ * visita controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::visita.visita');
---
+++ backend/src/api/visita/services/visita.ts
@@ -0,0 +1,7 @@
+/**
+ * visita service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::visita.visita');
